692caa778357fba490b7d218b282419d2d56938c
Viola Josehine Hestbech <viol1397@stud.kea.dk>
2021-11-09 22:52:20 +0100

    YEW ALMOST THERE

--- js.js
+++ js.js
@@ -30,3 +30,68 @@ gsap
   .to("#quote", {
     color: "#f2ede2",
   });
+
+//I wanna
+let i = 0;
+
+const randomizeText = () => {
+  const phrase = document.querySelector(".random-word");
+  const compStyles = window.getComputedStyle(phrase);
+  const animation = compStyles.getPropertyValue("animation");
+  const animationTime = parseFloat(animation.match(/\d*[.]?\d+/)) * 1000;
+
+  const phrases = [
+    "Graphic design",
+    "Wordpress",
+    "Motion graphics",
+    "Video editing",
+    "Design theory",
+    "Photography",
+    "GIF's",
+    "Photo editing",
+    "Premiere pro",
+    "Ideation and concept development",
+    "Group work",
+    "Media and society",
+  ];
+
+  i = randomNum(i, phrases.length);
+  const newPhrase = phrases[i];
+
+  setTimeout(() => {
+    phrase.textContent = newPhrase;
+  }, 400); // time to allow opacity to hit 0 before changing word
+};
+
+const randomNum = (num, max) => {
+  let j = Math.floor(Math.random() * max);
+
+  // ensure diff num every time
+  if (num === j) {
+    return randomNum(i, max);
+  } else {
+    return j;
+  }
+};
+
+const getAnimationTime = () => {
+  const phrase = document.querySelector(".random-word");
+  const compStyles = window.getComputedStyle(phrase);
+  let animation = compStyles.getPropertyValue("animation");
+
+  // firefox support for non-shorthand property
+  animation != ""
+    ? animation
+    : (animation = compStyles.getPropertyValue("-moz-animation-duration"));
+
+  // webkit support for non-shorthand property
+  animation != ""
+    ? animation
+    : (animation = compStyles.getPropertyValue("-webkit-animation-duration"));
+
+  const animationTime = parseFloat(animation.match(/\d*[.]?\d+/)) * 1000;
+  return animationTime;
+};
+
+randomizeText();
+setInterval(randomizeText, getAnimationTime());
